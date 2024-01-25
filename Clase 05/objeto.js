b66d98ae11d3916a2cc55ac25968d6bb7e02da48
Cerylion <fullthrottle.moran@gmail.com>
2024-01-24 19:05:08 -0600

    A lot of array methods, prettier, eslint and more...

--- Clase 05/objeto.js
+++ Clase 05/objeto.js
@@ -1,4 +1,4 @@
-const saludo = 'Hola-Mundo-Con-Mas-Palabras'
+/* const saludo = 'Hola-Mundo-Con-Mas-Palabras'
 
 console.log(saludo.split('-')) // con esto quito los espacios
 console.log(saludo)
@@ -8,12 +8,12 @@ console.log(saludo)
 console.log(saludoArray)
 console.log(saludoArray.sort())
 saludoArray.sort()
-console.log(saludoArray)
+console.log(saludoArray) */
 
 
 // tarea : funcion que sepa si una palabra o frase es un palindromo .reverse() en la palabra con .toLowerCase()
 // funcio nque quite los espacio s de un String
-
+/*
 let word = "deified"
 let otherWord = "Intensity"
 const phrase = "Is This The Real World?"
@@ -34,3 +34,21 @@ numArray.push(2)
 console.log(numArray)
 
 console.log(word.toUpperCase())
+ */
+
+
+/* for (num of nums) {
+  newNum.push(num*2)
+  console.log(newNum)
+} */
+const nums = [1, 2, 3, 4, 5, 6, 7, 8]
+
+const doubleNum = nums.reduce((prev, curr) => {
+  prev.push(curr*2)
+  return prev
+}, []
+)
+console.log(doubleNum)
+
+const double = []
+// console.log(newNum)
